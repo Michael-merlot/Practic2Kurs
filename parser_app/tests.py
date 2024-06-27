7d417b2ec348ac94d78b8a239dd952fa285b3600
miche <holevhuik@yandex.ru>
2024-06-27 19:27:10 +0300

    Add README.md and add tests

--- parser_app/tests.py
+++ parser_app/tests.py
@@ -1,3 +1,32 @@
-from django.test import TestCase
+# parser_app/tests.py
 
-# Create your tests here.
+from django.test import TestCase
+from .models import JobVacancy
+
+class JobVacancyModelTest(TestCase):
+
+    def setUp(self):
+        JobVacancy.objects.create(
+            title="Python Developer",
+            company="Example Company",
+            location="Москва",
+            description="Some description",
+            requirements="Some requirements",
+            conditions="Full-time"
+        )
+
+    def test_job_vacancy_creation(self):
+        job = JobVacancy.objects.get(title="Python Developer")
+        self.assertEqual(job.company, "Example Company")
+        self.assertEqual(job.location, "Москва")
+        self.assertEqual(job.description, "Some description")
+        self.assertEqual(job.requirements, "Some requirements")
+        self.assertEqual(job.conditions, "Full-time")
+
+class JobVacancyViewTest(TestCase):
+
+    def test_fetch_jobs_view(self):
+        response = self.client.get('/parser/fetch-jobs/')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Available Jobs")
+
